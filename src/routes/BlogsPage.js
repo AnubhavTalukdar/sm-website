a5baeda418476c9d6478bc7c9f0b4e01b52d1343
Subhradeep Das <dassubhra1998@gmail.com>
2022-01-05 15:06:54 +0530

    Back to top button added to Blogs page

--- src/routes/BlogsPage.js
+++ src/routes/BlogsPage.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Blogs from "../components/Blogs"
 import FloatingButton from "../components/FloatingButton";
+import BackToTop from "../components/BackToTop.js";
 
 function BlogsPage(){
     return(
         <>
         <Blogs />
         <FloatingButton />
+        <BackToTop />
         </>
     )
 }
